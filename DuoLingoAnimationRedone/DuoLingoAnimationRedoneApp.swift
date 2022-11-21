c4d50e6e665ad0f69a199719f35fa33c54a0ded6
boubelpi <113375173+boubelpi@users.noreply.github.com>
2022-11-21 13:58:49 +0100

    Initial Commit

---
+++ DuoLingoAnimationRedone/DuoLingoAnimationRedoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuoLingoAnimationRedoneApp.swift
+//  DuoLingoAnimationRedone
+//
+//  Created by Pavel on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DuoLingoAnimationRedoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
