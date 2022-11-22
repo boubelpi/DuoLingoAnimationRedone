7e9e13334a9da69aaf2500a1cb16e37a8f03c188
boubelpi <113375173+boubelpi@users.noreply.github.com>
2022-11-22 17:05:40 +0100

    Text updating added

--- DuoLingoAnimationRedone/ContentView.swift
+++ DuoLingoAnimationRedone/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 4, on: .main, in: .common).autoconnect()
+    @State var current_text = txt.find_text_under_animation()
     var body: some View {
         VStack {
             Text("LOADING...")
                 .font(.title2)
                 .fontWeight(.regular)
                 .foregroundColor(.gray)
-            Text(txt.find_text_under_animation())
+            Text(current_text)
                 .font(.body)
-                .padding(2)
-        }
+                .padding(5)
+                .onReceive(timer) {_ in
+                    self.current_text = txt.find_text_under_animation()
+                    
+                }
+                }
         .padding()
     }
 }
