33a132db12b199295f3a7830f2c06ada125f6bf2
boubelpi <113375173+boubelpi@users.noreply.github.com>
2022-11-23 17:48:08 +0100

    SpriteKit animation added

--- DuoLingoAnimationRedone/ContentView.swift
+++ DuoLingoAnimationRedone/ContentView.swift
@@ -6,12 +6,53 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+class GameScene : SKScene {
+    var player : SKSpriteNode!
+    private var playerAtlas : SKTextureAtlas {
+        return SKTextureAtlas(named : "Sprites")
+    }
+    private var playerTexture : SKTexture {
+        return playerAtlas.textureNamed("ani5")
+    }
+    private func setupPlayer() {
+        player = SKSpriteNode(texture : playerTexture, size : CGSize(width : 70, height : 70))
+        player.position = CGPoint(x : frame.width / 2, y : frame.height / 2)
+        addChild(player)
+    }
+    private var playerTextures : [SKTexture] {
+        return [
+            playerAtlas.textureNamed("ani5"),
+            playerAtlas.textureNamed("ani6"),
+            playerAtlas.textureNamed("animaz3"),
+            playerAtlas.textureNamed("anima1")
+        ]
+    }
+    func startAnimations() {
+        let anim = SKAction.animate(with : playerTextures, timePerFrame : 0.2)
+        player.run(SKAction.repeatForever(anim), withKey : "playerIdleAnimations")
+    }
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.white
+        self.setupPlayer()
+        self.startAnimations()
+    }
+}
 
 struct ContentView: View {
+    var scene : SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width : 216, height : 216)
+        scene.scaleMode = .fill
+        return scene
+    }
     let timer = Timer.publish(every: 4, on: .main, in: .common).autoconnect()
     @State var current_text = txt.find_text_under_animation()
     var body: some View {
         VStack {
+            SpriteView(scene : self.scene)
+                .frame(width : 256, height : 256)
             Text("LOADING...")
                 .font(.title2)
                 .fontWeight(.regular)
@@ -23,8 +64,8 @@ struct ContentView: View {
                     self.current_text = txt.find_text_under_animation()
                     
                 }
-                }
-        .padding()
+        }
+        .padding(30)
     }
 }
 
